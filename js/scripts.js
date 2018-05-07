c3cdd84859afe075dd5210d2245d1bfefb31603b
Dorota Kazak <dorota.kazak@gmail.com>
2018-05-07 16:28:54 +0200

    Add half length

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,6 @@ var textUpperCased = dinosaur.toUpperCase();
 console.log(textUpperCased);
 var textCharsAfter = text.replace('Velociraptor', textUpperCased);
 console.log(textCharsAfter);
-console.log(textCharsAfter.length / 2);
-var halfText = textCharsAfter.substr(0, 72);
+/*console.log(textCharsAfter.length / 2);*/
+var halfText = textCharsAfter.substr(0,(textCharsAfter.length / 2));
 console.log(halfText);
